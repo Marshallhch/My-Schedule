a9c1075c0574c1ca14949db7521a07dabeb4f424
Marshallhch <Marshallhch@gmail.com>
2021-02-08 16:07:18 +0900

    02/05 그리드 레이아웃, 이지파이챠트 플러그인 적용 완료

--- index.php
+++ index.php
@@ -1,33 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Schedule Dashboard</title>
 
-    <!-- CSS Style Link -->
-    <link rel="stylesheet" href="/myschedule/css/reset.css" />
-    <link rel="stylesheet" href="/myschedule/css/style.css" />
-  </head>
-  <body>
-    <!-- All Contents are wrapped inside wrap class -->
-    <div class="wrap">
-      <!-- header -->
-      <header>
-        <div class="center headerWrap">
-          <span class="hollow"></span>
-          <h1>Schedule Dashboard</h1>
-          <div class="mIcon">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Schedule Dashboard</title>
+
+  <!-- CSS Style Link -->
+  <link rel="stylesheet" href="/myschedule/css/reset.css" />
+  <link rel="stylesheet" href="/myschedule/css/style.css" />
+</head>
+
+<body>
+  <!-- All Contents are wrapped inside wrap class -->
+  <div class="wrap">
+    <!-- header -->
+    <header>
+      <div class="center headerWrap">
+        <span class="hollow"></span>
+        <h1>Schedule Dashboard</h1>
+        <div class="mIcon">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-      </header>
+      </div>
+    </header>
 
-      <!-- Container -->
-      <div class="container"></div>
+    <!-- Container -->
+    <div class="center">
+      <div class="container">
+        <div class="item">
+          <section class="graph-circle">
+            <div class="circle-graph-container">
+              <div class="circle-graph easyPieChart" data-percent="60">
+                <p>60%</p>
+                <!-- <canvas width="269" height="269" style="width: 150px; height: 150px;"></canvas> -->
+              </div>
+            </div>
+            <p class="totalTit">Total Process Rates</p>
+          </section>
+        </div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+      </div>
     </div>
-  </body>
+  </div>
+
+  <!-- script files load -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/jquery.easypiechart.min.js"></script>
+  <script src="/myschedule/js/piechart.js"></script>
+</body>
+
 </html>
---
+++ js/piechart.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.circle-graph').easyPieChart({
+    scaleColor: false,
+    lineWidth: 40,
+    lineCap: 'butt',
+    barColor: 'indigo',
+    trackColor: '#cccccc' ,
+    size: 250,
+    animate: 800
+  });
+});

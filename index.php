0b465d5d572aaf9956af613a8f0a33fef406ca77
Marshallhch <Marshallhch@gmail.com>
2021-02-08 12:41:39 +0900

    02/08 일정 관리 어플리케이션 헤더 완성

--- index.php
+++ index.php
@@ -1,53 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Schedule Dashboard</title>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Schedule Dashboard</title>
-  <link rel="stylesheet" href="/myschedule/css/style.css">
-</head>
-
-<body>
-  <div class="wrap">
-    <header>
-      <div class="center headerCenter">
-        <span class="hollow"></span>
-        <h1>Schedule Dashboard</h1>
-        <div class="mIcon">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </header>
-    <div class="center">
-      <div class="container">
-        <div class="item">
-          <div class="graph-circle">
-            <div class="circle-graph-container">
-              <div class="circle-graph easyPieChart" data-percent="30">
-                <p class="totalPercent">30%</p>
-                <!-- <canvas width="300" height="300"></canvas> -->
-              </div>
-            </div>
-            <p class="totalTit">Total Process Rates</p>
+    <!-- CSS Style Link -->
+    <link rel="stylesheet" href="/myschedule/css/reset.css" />
+    <link rel="stylesheet" href="/myschedule/css/style.css" />
+  </head>
+  <body>
+    <!-- All Contents are wrapped inside wrap class -->
+    <div class="wrap">
+      <!-- header -->
+      <header>
+        <div class="center headerWrap">
+          <span class="hollow"></span>
+          <h1>Schedule Dashboard</h1>
+          <div class="mIcon">
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
-
         </div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-      </div>
+      </header>
+
+      <!-- Container -->
+      <div class="container"></div>
     </div>
-  </div>
-  </div>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/jquery.easypiechart.min.js"></script>
-  <script src=/myschedule/js/custom.js></script>
-</body>
-
+  </body>
 </html>
